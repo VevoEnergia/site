0997dd5df6ce3ddf84f3426fd26c1bd6301b3e06
Joaoaalves <joaoaa21@hotmail.com>
2024-12-05 17:48:10 -0300

    feat(ui): create modal component for price simulation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,11 @@
+import Simulacao from "./Simulacao";
+
 export default function Hero(){
     return <section className="w-screen h-[calc(100vh-80px)] py-16 bg-gradient-to-br from-white to-zinc-500 flex flex-col items-center justify-between">
         <div className="bg-white rounded-full py-2 px-4">
             <h1 className="font-bold text-2xl text-text">Quem faz as contas, escolhe <strong>VEVO</strong>!</h1>
         </div>
-        <button className="shadow-md bg-black  font-light text-lg text-white rounded-full px-4 py-2">Faça uma simulação</button>
+
+        <Simulacao />
     </section>
 }
---
+++ src/components/Simulacao.tsx
@@ -0,0 +1,23 @@
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+export default function Simulacao(){
+    return (
+        
+        <Dialog>
+        <DialogTrigger className="shadow-md bg-black text-white font-light text-lg rounded-full px-4 py-2">Faça uma simulação</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Faça sua simulação</DialogTitle>
+            <DialogDescription className="w-80 h-96">
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    )
+}
