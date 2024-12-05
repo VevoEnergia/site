8c49b91d32f8756d43f7dd351384b4763cd01ca0
Joaoaalves <joaoaa21@hotmail.com>
2024-12-05 19:26:58 -0300

    feat(ui): improved question spacing and arrow size

--- src/components/FAQ/Question.tsx
+++ src/components/FAQ/Question.tsx
@@ -9,8 +9,8 @@ interface QuestionProps{
 export default function Question({question, answer}: QuestionProps){
     return (
         <div className="px-12 py-4 w-3/4 border shadow-md rounded-lg even:ms-auto cursor-pointer hover:shadow-lg transition-all duration-300">
-            <div className="flex items-center gap-x-8">
-                <IoArrowDown />
+            <div className="flex items-center gap-x-4 mb-4">
+                <IoArrowDown className="text-3xl"/>
                 <Subtitle>
                     {question}
                 </Subtitle>
