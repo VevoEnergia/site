597d9eab3c9e999b90d2decb31181049665cbba7
Joaoaalves <joaoaa21@hotmail.com>
2025-05-19 13:30:51 -0300

    fix: fixed CTA Button position

--- src/components/ui/cta-button.tsx
+++ src/components/ui/cta-button.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
 interface CTAButtonProps {
   text?: string;
   variant?: "filled" | "outline" | "ghost" | "secondary";
@@ -11,14 +12,20 @@ export default function CTAButton({
   variant = "filled",
   className,
 }: CTAButtonProps) {
+  const router = useRouter();
+
+  const navigateContact = () => {
+    router.push("#contact-form");
+  };
+
   return (
-    <a
+    <button
       data-variant={variant}
       className={cn(
         "drop-shadow-[0px_4px_4px_#00000040] data-[variant=filled]:bg-primary data-[variant=filled]:text-secondary data-[variant=outline]:bg-transparent data-[variant=outline]:text-primary data-[variant=ghost]:bg-white data-[variant=ghost]:text-secondary rounded-full !text-2xl 2xl:text-3xl py-3 px-4 2xl:py-4 2xl:px-5 font-bold w-2/3 min-w-fit text-center hover:scale-105 transition-all duration-300 data-[variant=secondary]:bg-secondary data-[variant=secondary]:text-white",
         className
       )}
-      href="#contact-form"
+      onClick={navigateContact}
     >
       {text.slice(0, text.length - 1)}
       <span
@@ -27,6 +34,6 @@ export default function CTAButton({
       >
         {text[text.length - 1]}
       </span>
-    </a>
+    </button>
   );
 }
